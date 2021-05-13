075d7d5e72cae706d3bc81b8e035d5cea9f39521
Sunny  Prakash <sunny.pprakash@gmail.com>
2021-05-13 12:04:28 +0530

    change

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,16 @@
-
+import React from "react";
+import "../styles/Child.css";
+const Selection = ({ applyColor, nextBackground }) => {
+    return (
+        <div
+            className="fix-box"
+            style={nextBackground}
+            onClick={() => {
+                applyColor();
+            }}
+        >
+            <h2 className="subheading">{"Selection"}</h2>
+        </div>
+    );
+};
+export default Selection;
